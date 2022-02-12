72475b1ef977f9a4db0a0b70f157df1a91cbaf78
Nasser Ghiasi <nasserghiasi@gmail.com>
2022-02-12 18:14:47 +0330

    feat: home routed component

---
+++ src/routes/home/index.tsx
@@ -0,0 +1,56 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Grid } from '@mui/material';
+import { Link, Route, Routes } from 'react-router-dom';
+import FacilityModel, { FacilityEntity } from 'models/Facility';
+import FacilityCard from 'components/FacilityCard';
+import Spinner from 'components/Spinner';
+import Facility from '../facility';
+
+function Home() {
+  const [facilities, setFacilities] = useState<FacilityEntity[]>();
+
+  const fetchList = async () => {
+    const { data } = await FacilityModel.getList();
+    setFacilities(data);
+  };
+
+  const refetchList = useCallback(() => {
+    setFacilities(undefined);
+    fetchList();
+  }, []);
+
+  useEffect(() => {
+    fetchList();
+  }, []);
+
+  return (
+    <div>
+      <Grid
+        container
+        width="100%"
+        justifyContent="space-between"
+        alignItems="center">
+        <h1>Facility List</h1>
+
+        <Link to="/facilities/new">Create facility</Link>
+      </Grid>
+
+      {facilities && (
+        <Grid container width="100%" alignItems="center" gap={3}>
+          {facilities.map(facility => (
+            <FacilityCard key={facility.id} data={facility} />
+          ))}
+        </Grid>
+      )}
+
+      {!facilities && <Spinner />}
+
+      <Routes>
+        <Route path=":id" element={<Facility onSubmit={refetchList} />} />
+        <Route path="new" element={<Facility onSubmit={refetchList} />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default Home;
